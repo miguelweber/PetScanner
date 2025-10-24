ec9f257c148738ca777332f21bf9e803765e1cfc
Miguel Weber <webercarvalhaes@gmail.com>
2025-10-24 17:29:49 -0300

    Abandonar o NodeJS

--- routes/autenticacao.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registrar, login } = require('../controllers/autenticacaoController');
-
-router.post('/registrar', registrar);
-router.post('/login', login);
-
-module.exports = router;
